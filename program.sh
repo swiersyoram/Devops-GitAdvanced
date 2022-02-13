8eb3a80fce72e97e2de4663ce451b0f039c91ca7
Wouter Peetermans <wouter@wouterpeetermans.com>
2022-02-13 23:25:37 +0100

    Add personal greeting to program

--- program.sh
+++ program.sh
@@ -5,5 +5,13 @@ echo "Hello World!";
 echo "Feature 2";
 =======
 read -p "Give your name: " name;
+<<<<<<< HEAD
+<<<<<<< HEAD
 echo $name;
 >>>>>>> 5bd20d8 (Add print name functionality to program)
+=======
+echo "Hello $name. Greetings to you my friend";
+>>>>>>> 1e3c4a0 (Add personal greeting to program)
+=======
+echo "Hello $name. Greetings to you my friend.";
+>>>>>>> 75c203a (Fix typo)
